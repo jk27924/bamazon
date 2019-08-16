d27e499e275af7c84b8ce738fd0d81b85afd123f
Jihong Kim <jk279242naver.com>
2019-08-15 20:56:03 -0500

    created bamazonCustomer.js, and declared needed variables for mysql and inquirer. Added table as well.

---
+++ bamazonCustomer.js
@@ -0,0 +1,23 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var connection = mysql.createConnection({
+    host: localhost,
+    port: 8889,
+    user: "root",
+    password: "root",
+    database: "bamazon_db"
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    afterConnection();
+});
+
+function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+  }
