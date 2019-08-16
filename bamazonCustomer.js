f965153430c3a61045b31e89d6d1cfa187ae4f1f
Jihong Kim <jk279242naver.com>
2019-08-16 00:18:45 -0500

    declared another function called displayItems from my product. And declared another variable called table with for loop.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,8 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
 var connection = mysql.createConnection({
-    host: localhost,
+    host: "localhost",
     port: 8889,
     user: "root",
     password: "root",
@@ -11,13 +12,29 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId);
-    afterConnection();
+    displayItems();
 });
 
-function afterConnection() {
+function displayItems() {
     connection.query("SELECT * FROM products", function(err, res) {
-      if (err) throw err;
-      console.log(res);
-      connection.end();
+        if (err) throw err;
+        console.log(res);
+
+        var table = new Table ({
+            head: ["Product ID", "Product Name", "Department Name", "Price", "Quantity"],
+        });
+
+        for (var i = 0; i < res.length; i++) {
+            table.push (
+                [res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]
+            )
+        };
+
+        console.log(table.toString());
+        purchasePrompt();
     });
-  }
+};
+
+
+
+
